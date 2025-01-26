9201e543b8b46598945e83608bcdca5854e6b122
Coinsinical <88478301+Coinsinical@users.noreply.github.com>
2025-01-26 23:03:18 +0800

    fix libsodium error

--- include/php-8.2.sh
+++ include/php-8.2.sh
@@ -41,7 +41,7 @@ Install_PHP82() {
   fi
 
   if [ ! -e "/usr/local/lib/pkgconfig/libargon2.pc" ]; then
-    tar xzf argon2-${argon2_ver}.tar.gz
+    tar xzf argon2-${argon2_ver}.tar.gz && mv phc-winner-argon2-${argon2_ver} argon2-${argon2_ver}
     pushd argon2-${argon2_ver} > /dev/null
     make -j ${THREAD} && make install
     [ ! -d /usr/local/lib/pkgconfig ] && mkdir -p /usr/local/lib/pkgconfig
@@ -52,7 +52,7 @@ Install_PHP82() {
 
   if [ ! -e "/usr/local/lib/libsodium.la" ]; then
     tar xzf libsodium-${libsodium_up_ver}.tar.gz
-    pushd libsodium-${libsodium_up_ver} > /dev/null
+    pushd libsodium-statble > /dev/null
     ./configure --disable-dependency-tracking --enable-minimal
     make -j ${THREAD} && make install
     popd > /dev/null
